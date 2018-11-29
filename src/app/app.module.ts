557c9ce82108be6167292ae8e3f0859276dceca8
Railan Barbosa <railan.barbosa@osf-commerce.com>
2018-11-28 22:21:26 -0300

    Generate components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './components/form/form.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ActionsComponent } from './components/actions/actions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    HeaderComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule
