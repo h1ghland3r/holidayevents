119ad095d20bc976d5344c5504e645c351f13875
Railan Barbosa <railan.barbosa@osf-commerce.com>
2018-11-28 22:58:26 -0300

    Define form script to handle the submit

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { HolidayService } from '../../shared/holiday.service';
 
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+    selector: 'app-form',
+    templateUrl: './form.component.html',
+    styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+    private name: string;
+    private date: string;
+    private location: string;
+    private note: string;
 
-  ngOnInit() {
-  }
+    constructor(private holidayService: HolidayService) {
+        this.name = '';
+        this.date = '';
+        this.location = '';
+        this.note = '';
+    }
 
+    ngOnInit() {
+    }
+
+    private save(): void {
+        this.holidayService.add(this.name);
+        this.holidayService.add(this.date);
+        this.holidayService.add(this.location);
+        this.holidayService.add(this.note);
+
+        // then clear the values after submit
+        this.name = '';
+        this.date = '';
+        this.location = '';
+        this.note = '';
+    }
 }
