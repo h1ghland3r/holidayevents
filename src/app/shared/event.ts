ec7b358191d74c8fe4031a5d1784e7cb8577d8dd
Railan Barbosa <railan.barbosa@osf-commerce.com>
2018-11-28 23:36:55 -0300

    Update in the class

--- src/app/shared/event.ts
+++ src/app/shared/event.ts
@@ -2,14 +2,14 @@ export class Event {
     id: number;
     name: string;
     date: string;
-    office: string;
+    location: string;
     note: string;
 
-    constructor(id: number, name: string, date: string, office: string, note: string) {
+    constructor(id: number, name: string, date: string, location: string, note: string) {
         this.id = id;
         this.name = name;
         this.date = date;
-        this.office = office;
+        this.location = location;
         this.note = note;
     }
 }
